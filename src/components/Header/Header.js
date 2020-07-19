9284c98b75830efdb99fe19e3c084d03a454968a
Amkuna <rekassaul@gmail.com>
2020-07-19 12:14:23 +0300

    Began work on Header component: added hero image and text

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+const Header = () => {
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.header__content}>
+                <h1 className={styles.header__heading}>I'm Saulius Rekašius.</h1>
+                <p className={styles.header__paragraph}>
+                    Web Developer
+                </p>
+            </div>
+
+        </header>
+    );
+};
+
+export default Header;
