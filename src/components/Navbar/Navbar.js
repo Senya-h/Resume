23b6e134d6248e17725713425b08a822bb58ac21
Amkuna <rekassaul@gmail.com>
2020-07-19 17:22:25 +0300

    Added temporary navbar and added linear gradient for header

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styles from './Navbar.module.scss';
+
+const Navbar = () => {
+
+    
+
+    const setActive = (e) => {
+        console.log(e.target);
+        e.target.classList.add(styles.navbar__active);
+    }
+
+    return (
+        <nav className={styles.navbar}>
+            <ul className={styles.navbar__list}>
+                <li className={`${styles.navbar__item} ${styles.navbar__active}`}>
+                    <a 
+                        className={styles.navbar__link}
+                        href="#home"
+                        onClick={setActive}
+                        >Home</a>
+                    <span className={styles.navbar__delimeter}>/</span>
+                </li>
+                <li className={styles.navbar__item}>
+                    <a 
+                        className={styles.navbar__link}
+                        href="#about"
+                        onClick={setActive}
+                        >About</a>
+                    <span className={styles.navbar__delimeter}>/</span>
+                </li>
+                <li className={styles.navbar__item}>
+                    <a className={styles.navbar__link} href="#resume">Resume</a>
+                    <span className={styles.navbar__delimeter}>/</span>
+                </li>
+                <li className={styles.navbar__item}>
+                    <a className={styles.navbar__link} href="#works">Works</a>
+                    <span className={styles.navbar__delimeter}>/</span>
+                </li>
+                <li className={styles.navbar__item}>
+                    <a className={styles.navbar__link} href="#contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Navbar;
