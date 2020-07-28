7b45392a991bd29a10adbbba58d09341f76245da
Amkuna <rekassaul@gmail.com>
2020-07-28 11:10:46 +0300

    Improved header design

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Header.module.scss';
+import ParticlesBg from 'particles-bg';
 
 const Header = () => {
 
     return (
         <header id="home" className={styles.header}>
+            <ParticlesBg type="cobweb" color="#01ffb3" /> {/*lines, circle,  */}
             <div className={styles.header__content}>
-                <h1 className={styles.header__heading}>Hey, I'm Saulius Rekašius.</h1>
+                <h1 className={styles.header__heading}>Hey, I'm Saulius Rekašius</h1>
                 <p className={styles.header__paragraph}>
                     Web Developer
                 </p>
